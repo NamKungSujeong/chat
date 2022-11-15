a51d18a263ad6bf52e9ca1bd214e6fe571c2d0b0
Sujeong <nksj98@naver.com>
2022-11-15 21:05:11 +0900

    first commit

---
+++ 18_socket/chat.js
@@ -0,0 +1,93 @@
+const { openDelimiter } = require("ejs");
+const express = require("express");
+const app = express();
+// socket은 express가 아닌 http 모듈에 연결해야 사용 가능
+const http = require("http").Server(app);
+const io = require("socket.io")(http); // http-socket 연결
+const PORT = 8000;
+
+app.set("view engine", "ejs");
+app.use("/views", express.static(__dirname + "/views"));
+app.use("/static", express.static(__dirname + "/static"));
+
+app.get("/", (req, res) => {
+  res.render("chat");
+});
+
+const nickArray = {}; // 유저 목록
+
+// io.on()
+// : socket과 관련된 통신작업을 처리
+io.on("connection", (socket) => {
+  // "connection" event
+  // : 클라이언트가 접속했을 때 발생, 콜백으로 socket 객체를 제공!
+  /*
+  console.log("**** ⭕ Server Socket Connected >> ", socket.id);
+  // socket.id: 소켓 고유 아이디 -> socket은 웹 페이지 별로 생김!!
+
+  // [실습42]
+  
+  socket.on("hello", (data) => {
+    // console.log('socket on hello >> ', data);
+    console.log(`${data.who} : ${data.msg}`);
+    socket.emit("helloKr", { who: "hello", msg: "안녕~~~" });
+  });
+
+  socket.on("study", (data) => {
+    // console.log('socket on hello >> ', data);
+    console.log(`${data.who} : ${data.msg}`);
+    socket.emit("studyKr", { who: "study", msg: "공부~~~" });
+  });
+
+  socket.on("bye", (data) => {
+    // console.log('socket on hello >> ', data);
+    console.log(`${data.who} : ${data.msg}`);
+    socket.emit("byeKr", { who: "bye", msg: "빠이루~~~" });
+  });
+  */
+  // 실습 44 채팅 입장 안내
+  // io.emit("notice", `${socket.id.slice(0, 5)}님이 입장하셨습니다.`);
+
+  //실습 44-2 채팅창 입장 안내문 socket.id => nickname
+  socket.on("setNick", (nick) => {
+    console.log("socket on setNick >> ", nick);
+    // console.log(Object.values(nickArray).indexOf(nick));
+    //nickArray: {socket.id1: nick1, socket.id2: nick2, ....}
+    // -> Object.values(): [nick1, nick2, nick3...]
+    // -> indexOf() : nick이 존재하는지
+    if (Object.values(nickArray).indexOf(nick) > -1) {
+      // 닉네임 중복이 있다면
+      socket.emit("error", "이미 존재하는 닉네임입니다. 다시 입력해 주세요");
+    } else {
+      // 닉네임 중복이 없다면
+      nickArray[socket.id] = nick; // {socket.id : nick};
+      console.log("접속 유저 목록 >> ", nickArray);
+      io.emit("notice", `${nick}님이 입장하셨습니다.`);
+      socket.emit("entrySuccess", nick);
+    }
+  });
+
+  // 실습 44-3 접속자 퇴장시
+  // 'notice' 이벤트로 퇴장 공지
+
+  socket.on("disconnect", (nick) => {
+    // 1. socket.id 콘솔로그 찍기
+    console.log("disconnect >>> ", socket.id);
+    // 2. 전체공지(io) ('notice', 퇴장메세지('유저 닉네임 포함해서);
+    io.emit("notice", `${nickArray[socket.id]}님이 퇴장하셨습니다.`);
+    // 3. nickArray에서 해당 유저 삭제 (객체에서 key-value 쌍 삭제)
+    delete nickArray[socket.id];
+    // console.log(nickArray);
+  });
+
+  socket.on("send", (data) => {
+    console.log("socket on send >> ", data);
+    const sendData = { nick: data.myNick, msg: data.msg };
+    io.emit("newMessage", sendData);
+  });
+});
+
+// 주의!!!) 소켓을 사용하기 위해서는 http.listen()으로 포트를 열어야 함
+http.listen(PORT, () => {
+  console.log(`http://localhost:${PORT}`);
+});
